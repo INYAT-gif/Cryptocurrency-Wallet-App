5d9496b5e64ca00fa2cf92c2c2b329a6843a2953
inyat <inyat.nathani@gmail.com>
2024-05-03 13:37:40 +0200

    Initial commit

---
+++ src/main/java/se/inyat/App.java
@@ -0,0 +1,13 @@
+package se.inyat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
